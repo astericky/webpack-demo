d1181ce739566117a1ede0e6258d268964668954
Chris Sanders <csanders@sailthru.com>
2016-06-08 23:24:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# webpack-demo
+This is just for practice!!!
---
+++ app/component.js
@@ -0,0 +1,10 @@
+
+module.exports = function () {
+
+    var element = document.createElement('h1');
+
+    element.className = 'pure-button';
+    element.innerHTML = 'Hello World';
+
+    return element;
+};
